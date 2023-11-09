b5ffdc6352c4629402a5cf83df2fc05c43bdecc2
modevco <modevco05@gmail.com>
2023-11-09 21:50:14 +0500

    ozgartirish kiritdm

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,9 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
-import AboutUs from '../components/about/about'
 
 const About = () => {
   return (
     <Box>
-      <About />
       
     </Box>
   )
