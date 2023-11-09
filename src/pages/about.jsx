d8d6c336f67129cbcc467f36dd03073e2ee33615
modevco <modevco05@gmail.com>
2023-11-09 21:54:48 +0500

    yangi orgartirish

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <Box>
+    <div>
       About
-    </Box>
+    </div>
   )
 }
 
--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <Box>
-      Menu 
-    </Box>
+    <div>
+      Menu
+    </div>
   )
 }
 
